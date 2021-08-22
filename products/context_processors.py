aff67f8a44d694efa6ea6a29c3782f92a51a4261
vskarasev <karasev997@mail.ru>
2021-08-22 16:08:11 +0500

    откат корзины

--- products/context_processors.py
+++ products/context_processors.py
@@ -4,8 +4,7 @@ from baskets.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        # basket_list = Basket.objects.filter(user=request.user)
-        basket_list = request.user.basket.select_related()
+        basket_list = Basket.objects.filter(user=request.user)
 
     return {
         'basket': basket_list
