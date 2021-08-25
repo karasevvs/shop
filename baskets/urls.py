30e18427e5b49f3b4c1d67aeb2648493bd0415a3
vskarasev <karasev997@mail.ru>
2021-08-25 14:21:40 +0500

    cache basket

--- baskets/urls.py
+++ baskets/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('add/<int:product_id>/', basket_add, name='basket_add'),
     path('remove/<int:id>/', basket_remove, name='basket_remove'),
     path('edit/<int:id>/<int:quantity>/', basket_edit, name='basket_edit'),
-    path('view/', view, name='view'),
+    path('view/', cache_page(3600)(view, name='view')),
 ]
