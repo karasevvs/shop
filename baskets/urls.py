688d75b1289a95bc852c0574fc1a99918fff09a7
vskarasev <karasev997@mail.ru>
2021-08-25 14:13:40 +0500

    откат

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from baskets.views import basket_add, basket_remove, basket_edit, view
-from django.views.decorators.cache import cache_page
 
 app_name = 'baskets'
 
@@ -9,5 +8,5 @@ urlpatterns = [
     path('add/<int:product_id>/', basket_add, name='basket_add'),
     path('remove/<int:id>/', basket_remove, name='basket_remove'),
     path('edit/<int:id>/<int:quantity>/', basket_edit, name='basket_edit'),
-    path('view/', cache_page(3600)(view, name='view')),
+    path('view/', view, name='view'),
 ]
