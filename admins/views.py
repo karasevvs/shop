9eb6e37687e54a7ef30c4757e2d8f4b0c85c16c6
vskarasev <karasev997@mail.ru>
2021-08-30 13:52:22 +0500

    fix category

--- admins/views.py
+++ admins/views.py
@@ -113,6 +113,7 @@ class CategoryUpdateView(UpdateView):
             if discount:
                 print(f'применяется скидка {discount}% к товарам {self.object.name}')
                 self.object.product_set.update(price=F('price') * (1 - discount / 100))
+                db_profile_by_type(self.__class__, 'UPDATE', connection.queries)
 
         return super().form_valid(form)
 
